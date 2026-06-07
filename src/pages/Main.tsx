66e6fa4d86dee622af71abb320f66c4b3ae1d08e
liiked <linyuanqianpp@126.com>
2026-06-07 17:18:27 +0800

    feat: 去除多余页面

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,10 +3,6 @@ import "../App.css";
 import styles from "./Main.module.scss";
 import Section1 from "./Section1";
 import Section2 from "./Section2";
-import Section3 from "./Section3";
-import Section4 from "./Section4";
-import Section5 from "./Section5";
-import Section6 from "./Section6";
 
 export default function Main() {
   useEffect(() => {
